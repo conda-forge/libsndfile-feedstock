641ea97baa6c8d477d3d1b607539d28553e1b0ee
Erik Kastman <erik.kastman@gmail.com>
2018-05-01 11:37:42 -0400

    Change dir order from appveyor.yaml (no CMakeLists found in this folder currently?)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,21 +1,15 @@
 setlocal EnableDelayedExpansion
 
 :: Make a build folder and change to it.
-mkdir build
-cd build
+mkdir CMakeBuild
+cd CMakeBuild
 
 :: Configure using the CMakeFiles
-cmake -G "NMake Makefiles" ^
-      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_BUILD_TYPE:STRING=Release ^
-      ..
+cmake .. -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+         -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+         -DCMAKE_BUILD_TYPE:STRING=Release
+
 if errorlevel 1 exit 1
 
 :: Build!
-nmake
-if errorlevel 1 exit 1
-
-:: Install!
-nmake install
-if errorlevel 1 exit 1
+cmake --build . --target INSTALL
