cc95a8f991e699ea4313c46b6c60c55aae378493
Isuru Fernando <isuruf@gmail.com>
2018-11-17 01:32:53 -0600

    Add rpath-link

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$(uname)" == "Linux" ]
+then
+   export LDFLAGS="$LDFLAGS -Wl,-rpath-link,${PREFIX}/lib"
+fi
+
 ./configure prefix=$PREFIX
-make
+make -j${CPU_COUNT}
 make install
